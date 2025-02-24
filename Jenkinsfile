c491ad56168851119283a8532865e8236eb721b0
rahul <rahulsingh6811@gmail.com>
2025-02-24 18:03:05 +0530

    enable headless in jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,4 @@
-pipeline{
+pigitpeline{
 	agent any
 	
 	stages{
--- src/test/java/test/BaseClass.java
+++ src/test/java/test/BaseClass.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -14,7 +15,15 @@ public class BaseClass {
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\RAHUL\\eclipse-workspace\\JenkinsDemo\\Resources\\chromedriver.exe");
-		driver = new ChromeDriver();
+		boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+		ChromeOptions options = new ChromeOptions();
+		if (isHeadless) {
+			options.addArguments("--headless"); // Run in headless mode
+			options.addArguments("--disable-gpu"); // Optional, improves stability
+			options.addArguments("--window-size=1920,1080");
+		}
+
+		driver = new ChromeDriver(options);
 
 		driver.get("https://practicetestautomation.com/practice-test-login/");
 		driver.manage().window().maximize();
